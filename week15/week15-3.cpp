46c358408fd5423c4d030150f88e8a21b34fbc8b
gillhung <145088470+gillhung@users.noreply.github.com>
2024-05-31 11:42:22 +0800

    week15

---
+++ week15/week15-3.cpp
@@ -0,0 +1,7 @@
+int equalSubstring(char* s, char* t, int maxCost) {
+    int N=strlen(s); // 先知道字串長度
+    for(int i=0;i<N;i++){ //字串的迴圈
+        prntf("%c %c\n",s[i],t[i]);
+    }
+    return ans;
+}
