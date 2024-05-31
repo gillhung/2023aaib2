66ee62dbeec78fb4115ebda8e09167b94da83023
gillhung <145088470+gillhung@users.noreply.github.com>
2024-05-31 11:41:43 +0800

    week15

---
+++ week15-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        ans=0
+        N=len(s)
+        j=0 # 尾巴j
+        for i in range(N): 
+			print(s[i],t[i])
+        ans=0   
+
+        return ans
